8bebda57fb0d70fa524b2b13ce5acd5b5e4c0639
dokechin <user@user-PC.(none)>
2016-04-19 21:09:56 +0900

    add ie and phantomjs example

---
+++ steps/phantomjs/basic_outline_steps.pl
@@ -0,0 +1,20 @@
+#!perl
+
+use strict;
+use warnings;
+use utf8;
+
+use Test::BDD::Cucumber::StepFile;
+use Test::Selenium::Remote::Driver;
+
+Given qr/体重"(.+)"kgの牛/, sub {
+  my $sel = Test::Selenium::Remote::Driver->new(browser_name => 'phantomjs', remote_server_addr => 'localhost', port => 4444);
+  my $text = sprintf("//*[text()='体重%skgの牛']" , C->matches->[0]);
+  $sel->get("http://localhost:3000");
+  $sel->find_child_element( $sel->find_element("feed_type", "name"), $text, "xpath")->click;
+  S->{'sel'} = $sel;
+
+};
+
+
+
